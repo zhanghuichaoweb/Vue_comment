5be455652bc7df449fe8b868d14f63d38f114c95
zhanghuichao <zhanghuichaoweb@163.com>
2017-10-21 11:36:47 +0800

    first

---
+++ vue_test/src/components/app.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <add :addComment="addComment"></add>
+      <list :comments="comments" :deleteComment="deleteComment"></list>
+    </div>
+  </div>
+</template>
+
+<script>
+  import add from './add.vue'
+  import list from './list.vue'
+  export default {
+    data () {
+      return {
+        comments: [
+          {name: 'tom', content: 'Vue还不错'},
+          {name: 'bob', content: 'Vue真不错'}
+        ]
+      }
+    },
+    methods: {
+      addComment (comment) {
+        this.comments.unshift(comment)
+      },
+      deleteComment (index) {
+        this.comments.splice(index, 1)
+      }
+    },
+    components: {
+      add, list
+    }
+  }
+</script>
+
+<style>
+
+</style>
